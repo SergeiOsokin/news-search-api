a8da6a689e9d19ca3bf0aa4af05b6b355f657247
Sergei.Osokin <sergei.osokin.rf@mail.ru>
2020-06-25 17:08:30 +0300

    title without length

--- middlewares/validationArticle.js
+++ middlewares/validationArticle.js
@@ -16,7 +16,7 @@ const validationGetArticle = celebrate({
 const validationCreateArticle = celebrate({
   body: Joi.object().keys({
     keyword: Joi.string().required().min(2).max(30),
-    title: Joi.string().required().min(2).max(100),
+    title: Joi.string().required().min(2),
     text: Joi.string().required().min(2),
     date: Joi.string().required().min(2),
     source: Joi.string().required().min(2),
