7404e27b4ff52732818052440dc7e42c4401f060
Sergei.Osokin <sergei.osokin.rf@mail.ru>
2020-06-29 17:14:07 +0300

    New text errors

--- const.js
+++ const.js
@@ -1,9 +1,9 @@
-const resourceNotFound = 'Запрашиваемый ресурс не найден';
-const notFoundUserEmail = 'Не удалось найти пользователя с таким email';
+const resourceNotFound = 'Ресурс не найден';
+const notFoundUserEmail = 'Email в базе не найден';
 const wrongPasswordOrLogin = 'Не правильный логин или пароль';
 const alreadyExist = 'Email уже используется.';
 const errorInArticleId = 'Ошибка в присланном идентификаторе статьи';
-const needHeader = 'Нет заголовка. Требуется авторизация';
+const needHeader = 'Требуется авторизация';
 const badToken = 'Проблема с токеном';
 const dataNotFound = 'Данные в системе не найдены';
 const permissionText = 'Недостаточно прав';
